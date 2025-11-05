59e50318e822c60802a329a343c016060d8aadba
irfan ansari <irfa8318450619@gmail.com>
2025-11-05 23:28:08 +0530

    Deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import {ENV} from './lib/env.js';
+import path from 'path';
+
 
 const app=express();
 
+const __dirname=path.resolve();
+
+
 app.get('/',(req,res)=>{
     res.status(200).json({message:'user is running'});
+});
+
+app.get('/Home',(req,res)=>{
+    res.status(200).json({message:'Welcome to Home page'});
 })
+
+// make our app ready  for deployment
+if(ENV.NODE_ENV=='development'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+    })
+}
+
+
+
 app.listen(ENV.PORT,()=>{
     console.log("server is running on port",ENV.PORT);
 })
