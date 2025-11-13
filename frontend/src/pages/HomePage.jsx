fd092cb7790989374030216b0968accfe5a6e9be
irfan ansari <irfa8318450619@gmail.com>
2025-11-13 20:51:24 +0530

    frontend setup

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import {SignedOut, SignInButton, SignOutButton,SignedIn} from "@clerk/clerk-react"
 import toast from "react-hot-toast";
-
-export const HomePage = () => {
+import {Link} from "react-router"
+export const HomePage = ()=> {
   return (
-    <div>
-        <button  className='btn btn-secondary' onClick={()=>{toast.success("This is a toast")}}>click me</button>
-      <SignedOut>
-        <SignInButton  mode="modal">
-        <button className='btn btn-secondary'>Login</button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-       <SignOutButton>
-       <button className='btn btn-secondary'>LogOut</button> 
-        </SignOutButton>
-      </SignedIn>
+    <div className='bg-gradient-to-br from-base-100 via-base-200 to-base-300 '>
+        <p>dshbfsdy</p>
     </div>
 
   )
